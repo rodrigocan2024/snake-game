5ff3af86a87a8a5ba0fd039273100be537db0f4b
Rodrigo Cândido <rodrigocan2024@gmail.com>
2025-06-01 10:19:46 -0300

    Desenhando o grid

---
+++ src/constants.ts
@@ -0,0 +1,3 @@
+export const CANVAS_SIZE = 600;
+export const GRID_SIZE = 20;
+export const CELL_SIZE = CANVAS_SIZE / GRID_SIZE;
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import "./style.css";
 
+import { CANVAS_SIZE, CELL_SIZE, GRID_SIZE } from "./constants";
+
 class Game {
   canvas: HTMLCanvasElement;
   ctx: CanvasRenderingContext2D;
@@ -7,12 +9,32 @@ class Game {
   constructor() {
     this.canvas = document.getElementById("game-canvas") as HTMLCanvasElement;
     this.ctx = this.canvas.getContext("2d")!;
+    this.canvas.width = CANVAS_SIZE;
+    this.canvas.height = CANVAS_SIZE;
 
-    this.ctx.fillStyle = "rgb(200 0 0)";
-    this.ctx.fillRect(10, 10, 50, 50);
+    this.drawGrid();
+  }
 
-    this.ctx.fillStyle = "rgb(0 0 200 / 50%)";
-    this.ctx.fillRect(30, 30, 50, 50);
+  drawGrid() {
+    this.ctx.fillStyle = "#2c3e50";
+    this.ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
+
+    this.ctx.strokeStyle = "#ffffff33";
+    this.ctx.lineWidth = 1;
+
+    for (let i = 0; i < GRID_SIZE; i++) {
+      // Vertical lines
+      this.ctx.beginPath();
+      this.ctx.moveTo(i * CELL_SIZE, 0);
+      this.ctx.lineTo(i * CELL_SIZE, CANVAS_SIZE);
+      this.ctx.stroke();
+
+      // Horizontal lines
+      this.ctx.beginPath();
+      this.ctx.moveTo(0, i * CELL_SIZE);
+      this.ctx.lineTo(CANVAS_SIZE, i * CELL_SIZE);
+      this.ctx.stroke();
+    }
   }
 }
 
